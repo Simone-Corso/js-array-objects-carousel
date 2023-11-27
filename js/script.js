f931c250a5b6a1e04685f5fc6b0f1ee28fd8e9cb
Simone-Corso <scorso26@gmail.com>
2023-11-27 18:29:08 +0100

    sto creando un ciclo for per immagini

--- js/script.js
+++ js/script.js
@@ -30,3 +30,8 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+
+//Creo un ciclo for per immagini
+
+for (let i = 0;)
